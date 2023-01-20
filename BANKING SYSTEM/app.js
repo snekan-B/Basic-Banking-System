e9a13bfab106a92c33926f88fc1fb59aea59c152
Snekan <98229935+snekan-B@users.noreply.github.com>
2023-01-20 19:10:46 +0530

    Add files via upload

---
+++ BANKING SYSTEM/app.js
@@ -0,0 +1,17 @@
+// const form=document.getElementById('form');
+// const amount=document.getElementById('amount-box');
+
+// // form.addEventListener('submit',(e)=>
+// // {
+// //     if(amount.value.trim() === '')
+// //     {
+// //         console.log(amount.value);
+// //     e.preventDefault();
+// //     }
+
+   
+// // });
+
+// document.getElementById('form').addEventListener("submit", function(event){
+//     event.preventDefault()
+//   });
